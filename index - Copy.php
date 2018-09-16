149b1e469844ca5228271c538d814f6fef3a777f
Paolo Scapin <paolo.scapin@archifit.com>
2018-09-16 22:07:08 +0200

    copy ?

---
+++ index - Copy.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test</title>
+</head>
+
+<body >
+<?php
+	require_once 'header-session.php';
+?>
+<h2>Test</h2>
+<h3> <?php echo "User=$__username" ?> </h3>
+</body>
+</html>
